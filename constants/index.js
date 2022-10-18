ccf326a370fce4084d51fe945988e8d9923b32f5
reiineaa <80281865+reiineaa@users.noreply.github.com>
2022-10-18 18:12:26 +0600

    5

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xE883f275d8026A8D1F9Eb7bb5D8bBD8C40d7caD8";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://assignment2-9-icfn.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
